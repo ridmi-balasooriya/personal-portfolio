d393411fd7174fe4f49f9ee235cda429940b3d90
ridmi-balasooriya <ridmi.bizy@gmail.com>
2023-05-17 16:31:11 +0400

    Add NavBar.jsx for set navigation to Home Page.

---
+++ portfolio_frontend/personal_portfolio/src/pages/NavBar.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+const NavBar = () => {
+    return(
+        <nav>
+            <ul>
+                <li><Link to='/'>Home</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
