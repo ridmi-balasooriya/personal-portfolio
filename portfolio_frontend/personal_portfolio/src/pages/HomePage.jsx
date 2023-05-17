07418153c9af0c97a0522e80bd1fbbe7a330a67a
ridmi-balasooriya <ridmi.bizy@gmail.com>
2023-05-17 16:20:44 +0400

    Create Pages folder and Add HomePage.jsx for home. 1. Load personal portfolio data from backend and devide them to the relavent sections.    For Ex: get personal_info and send the personal info to PersonalData component. 2. Import home.css file for styling purposes.

---
+++ portfolio_frontend/personal_portfolio/src/pages/HomePage.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import '../static/css/home.css';
+import PersonalData from './PotfolioComponents/PersonalData';
+
+const HomePage = () => {
+
+    const [pPortfolio, setpPortfolio] = useState();
+    let personalInfo = {};
+    let contactDetails = {};
+    let socialMedia = {};
+    let workingExperiance = [];
+    let educationQualification = [];
+    let skills = [];
+    let softSkills = [];
+    let personalProjects = [];
+
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8000/api/data/')
+        .then(response => response.json())
+        .then(data => setpPortfolio(data))
+        .catch(error => {
+            alert(error)
+        })
+    }, [])
+    
+    if(pPortfolio){        
+        personalInfo = pPortfolio.portfolio.personal_info;
+        contactDetails = pPortfolio.portfolio.contact_details;
+        socialMedia = pPortfolio.portfolio.social_media;
+        workingExperiance = pPortfolio.portfolio.working_experiance;
+        educationQualification = pPortfolio.portfolio.educational_qualification;
+        skills = pPortfolio.portfolio.skills;
+        softSkills = pPortfolio.portfolio.soft_skills;
+        personalProjects = pPortfolio.portfolio.personal_projects
+    }
+    
+    
+    return (
+        <PersonalData data = {personalInfo}  />
+    );
+}
+
+export default HomePage;
